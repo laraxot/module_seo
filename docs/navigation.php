a152c333b23e90b989af75efe69ce1a5423f7752
Retype GitHub Action <retype@object.net>
2022-12-14 09:10:05 +0000

    Refreshes Retype-generated documentation.
    Process triggered by marco76tv.

---
+++ docs/navigation.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'Getting Started' => [
+        'url' => url('docs/getting-started'),
+        'children' => [
+            'Customizing Your Site' => url('docs/customizing-your-site'),
+            'Navigation' => url('docs/navigation'),
+            'Algolia DocSearch' => url('docs/algolia-docsearch'),
+            'Custom 404 Page' => url('docs/custom-404-page'),
+        ],
+    ],
+    'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
+];
