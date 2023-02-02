564bc31cd142776ad8a073ed22e7ae1be629ed84
marco76tv <marco.sottana@gmail.com>
2023-02-02 12:03:54 +0100

    up

--- Providers/SeoServiceProvider.php
+++ Providers/SeoServiceProvider.php
@@ -6,7 +6,8 @@ namespace Modules\Seo\Providers;
 
 use Modules\Xot\Providers\XotBaseServiceProvider;
 
-class SeoServiceProvider extends XotBaseServiceProvider {
+class SeoServiceProvider extends XotBaseServiceProvider
+{
     protected string $module_dir = __DIR__;
     protected string $module_ns = __NAMESPACE__;
     public string $module_name = 'seo';
