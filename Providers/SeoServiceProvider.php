d8efff4688f5c193707fbdbf155b0daaf7d1f45d
marco76tv <marco76tv@users.noreply.github.com>
2023-02-06 10:59:50 +0000

    Lint

--- Providers/SeoServiceProvider.php
+++ Providers/SeoServiceProvider.php
@@ -6,8 +6,7 @@ namespace Modules\Seo\Providers;
 
 use Modules\Xot\Providers\XotBaseServiceProvider;
 
-class SeoServiceProvider extends XotBaseServiceProvider
-{
+class SeoServiceProvider extends XotBaseServiceProvider {
     protected string $module_dir = __DIR__;
     protected string $module_ns = __NAMESPACE__;
     public string $module_name = 'seo';
