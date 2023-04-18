e8c65d59f9e14b2ca12cf0a16e3abf6737fa3700
marco76tv <marco.sottana@gmail.com>
2023-04-18 15:06:14 +0200

    up

--- Console/Command/SitemapGenerate.php
+++ Console/Command/SitemapGenerate.php
@@ -8,8 +8,7 @@ use Illuminate\Console\Command;
 use Spatie\Crawler\Crawler;
 use Spatie\Sitemap\SitemapGenerator;
 
-class SitemapGenerate extends Command
-{
+class SitemapGenerate extends Command {
     /**
      * The console command name.
      *
@@ -29,10 +28,10 @@ class SitemapGenerate extends Command
      *
      * @return mixed
      */
-    public function handle()
-    {
-        // dd(config('app.url'));
-        SitemapGenerator::create(config('app.url'))
+    public function handle() {
+        // dd(config('app.url'))
+        $url = strval(config('app.url'));
+        SitemapGenerator::create($url)
             ->configureCrawler(function (Crawler $crawler) {
                 $crawler->setMaximumDepth(3);
                 $crawler->ignoreRobots();
